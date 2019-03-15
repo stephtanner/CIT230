0eb387ee55da00a83b0430285fc5348b0f0abe81
Stephanie Tanner <46448150+stephtanner@users.noreply.github.com>
2019-03-15 14:53:45 -0600

    Add files via upload

--- assignments/lesson-10/api-test/scripts/weatherapi.js
+++ assignments/lesson-10/api-test/scripts/weatherapi.js
@@ -14,86 +14,10 @@ weatherRequest.send();
 weatherRequest.onload = function() {
     let weatherData = JSON.parse(weatherRequest.responseText);
     
-
-    document.getElementById("current-temp").innerHTML = weatherData;
+    var weatherTemp = weatherData;
+    
+    weatherTemp = weatherData.main.temp;
+    
+    document.getElementById("current-temp").innerHTML = weatherTemp;
+    
 }
-
-/*function populateWeather(jsonObj){
-    var currentWeather = jsonObj["properties"];
-    for (var i=0; i< currentWeather.length;i++){
-        var temperature = document.createElement('p');
-        temperature.textContent = properties[i].main;
-        
-        document.getElementById("current-temp").appendChild(temperature); 
-    }
-}*/
-
-
-/*
-//store url of json we want to retreive in a variable 
-var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json'; 
-
-//create request with new request object instance 
-var request = new XMLHttpRequest(); 
-
-//open new request using open() method 
-request.open('GET', requestURL); 
-
-//here we are setting the responseType to json 
-//tells that this should be converted behind sceens to JavaScript object 
-request.responseType = 'json'; 
-request.send(); 
-
-//waiting for the response to return from the server 
-request.onload = function() { 
-var prestonInfo = request.response; 
-populatePreston(prestonInfo) 
-} 
-
-function populatePreston(jsonObj){ 
-var towns = jsonObj["towns"]; 
-
-    for (var i= 0; i < towns.length; i++){
-        var name = towns[i].name;
-        if(name === "Preston") {
-            var townName = document.createElement('h1');
-            townName.textContent = towns[i].name;
-            var townMotto = document.createElement('h4');
-            townMotto.textContent = towns[i].motto;
-            var yearFounded = document.createElement('p');
-            yearFounded.textContent = 'Year Founded: ' + towns[i].yearFounded;
-            var population = document.createElement('p');
-            population.textContent = 'Current Population: ' + towns[i].currentPopulation;
-            var annRain = document.createElement('p');
-            annRain.textContent = 'Annual Rainfall: ' + towns[i].averageRainfall;
-            document.getElementById("prestonTownData").appendChild(townName); document.getElementById("prestonTownData").appendChild(townMotto); document.getElementById("prestonTownData").appendChild(yearFounded); document.getElementById("prestonTownData").appendChild(population); document.getElementById("prestonTownData").appendChild(annRain); 
-        }
-        else if (name === "Soda Springs"){
-                        var townName = document.createElement('h1');
-            townName.textContent = towns[i].name;
-            var townMotto = document.createElement('h4');
-            townMotto.textContent = towns[i].motto;
-            var yearFounded = document.createElement('p');
-            yearFounded.textContent = 'Year Founded: ' + towns[i].yearFounded;
-            var population = document.createElement('p');
-            population.textContent = 'Current Population: ' + towns[i].currentPopulation;
-            var annRain = document.createElement('p');
-            annRain.textContent = 'Annual Rainfall: ' + towns[i].averageRainfall;
-            document.getElementById("sodaSpringsTownData").appendChild(townName); document.getElementById("sodaSpringsTownData").appendChild(townMotto); document.getElementById("sodaSpringsTownData").appendChild(yearFounded); document.getElementById("sodaSpringsTownData").appendChild(population); document.getElementById("sodaSpringsTownData").appendChild(annRain);
-        }
-                else if (name === "Fish Haven"){
-                        var townName = document.createElement('h1');
-            townName.textContent = towns[i].name;
-            var townMotto = document.createElement('h4');
-            townMotto.textContent = towns[i].motto;
-            var yearFounded = document.createElement('p');
-            yearFounded.textContent = 'Year Founded: ' + towns[i].yearFounded;
-            var population = document.createElement('p');
-            population.textContent = 'Current Population: ' + towns[i].currentPopulation;
-            var annRain = document.createElement('p');
-            annRain.textContent = 'Annual Rainfall: ' + towns[i].averageRainfall;
-            document.getElementById("fishHavenTownData").appendChild(townName); document.getElementById("fishHavenTownData").appendChild(townMotto); document.getElementById("fishHavenTownData").appendChild(yearFounded); document.getElementById("fishHavenTownData").appendChild(population); document.getElementById("fishHavenTownData").appendChild(annRain);
-        }
-    }
-} 
-*/
--- assignments/lesson-10/scripts/individualtowndata.js
+++ assignments/lesson-10/scripts/individualtowndata.js
@@ -1,42 +1,39 @@
 //store url of json we want to retreive in a variable 
-var requestURL = 'https://byui-cit230.github.io/weather/data/towndata.json'; 
+var requestURL1 = 'https://byui-cit230.github.io/weather/data/towndata.json'; 
 
 //create request with new request object instance 
-var request = new XMLHttpRequest(); 
+var request1 = new XMLHttpRequest(); 
 
 //open new request using open() method 
-request.open('GET', requestURL); 
+request1.open('GET', requestURL1); 
 
 //here we are setting the responseType to json 
 //tells that this should be converted behind sceens to JavaScript object 
-request.responseType = 'json'; 
-request.send(); 
+request1.responseType = 'json'; 
+request1.send(); 
 
 //waiting for the response to return from the server 
-request.onload = function() { 
-var prestonInfo = request.response; 
-populatePreston(prestonInfo) 
+request1.onload = function() { 
+var prestonData = request1.response; 
+populatePrestonData(prestonData); 
 } 
 
-function populatePreston(jsonObj){ 
-var towns = jsonObj["towns"]; 
+function populatePrestonData(jsonObj){ 
+var allData = jsonObj["towns"]; 
 
-    for (var i= 0; i < towns.length; i++){
-        var name = towns[i].name;
+    for (var i= 0; i < allData.length; i++){
+        var name = allData[i].name;
         if(name === "Preston") {
-            var townName = document.createElement('h1');
-            townName.textContent = towns[i].name;
-            var townMotto = document.createElement('h4');
-            townMotto.textContent = towns[i].motto;
-            var yearFounded = document.createElement('p');
-            yearFounded.textContent = 'Year Founded: ' + towns[i].yearFounded;
-            var population = document.createElement('p');
-            population.textContent = 'Current Population: ' + towns[i].currentPopulation;
-            var annRain = document.createElement('p');
-            annRain.textContent = 'Annual Rainfall: ' + towns[i].averageRainfall;
-            document.getElementById("prestonTownData").appendChild(townName); document.getElementById("prestonTownData").appendChild(townMotto); document.getElementById("prestonTownData").appendChild(yearFounded); document.getElementById("prestonTownData").appendChild(population); document.getElementById("prestonTownData").appendChild(annRain);
+            var events0 = document.createElement('p');
+            var events1 = document.createElement('p');
+            var events2 = document.createElement('p');
+            
+            events0.textContent = allData[i].events[0];
+            events1.textContent = allData[i].events[1];
+            events2.textContent = allData[i].events[2];
+            document.getElementById("prestonEvents").appendChild(events0); document.getElementById("prestonEvents").appendChild(events1); document.getElementById("prestonEvents").appendChild(events2);
         }
-        else if (name === "Soda Springs"){
+        /*else if (name === "Soda Springs"){
                         var townName = document.createElement('h1');
             townName.textContent = towns[i].name;
             var townMotto = document.createElement('h4');
@@ -61,7 +58,6 @@ var towns = jsonObj["towns"];
             var annRain = document.createElement('p');
             annRain.textContent = 'Annual Rainfall: ' + towns[i].averageRainfall;
             document.getElementById("fishHavenTownData").appendChild(townName); document.getElementById("fishHavenTownData").appendChild(townMotto); document.getElementById("fishHavenTownData").appendChild(yearFounded); document.getElementById("fishHavenTownData").appendChild(population); document.getElementById("fishHavenTownData").appendChild(annRain);
-        }
+        }*/
     }
 } 
-
