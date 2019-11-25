a07d0fa49d17780c2eff5fa735e48aa49289cacf
Stephanie Tanner <46448150+stephtanner@users.noreply.github.com>
2019-11-25 14:24:31 -0700

    Add files via upload

--- assignments/templeinn/scripts/templeclosure.js
+++ assignments/templeinn/scripts/templeclosure.js
@@ -23,18 +23,17 @@ var townData = request.response;
     var templeID = " ";
      var temples = ["Kiev", "Preston", "Antonio", "Monticello"];
         for (var i = 0; i < temples.length; i++){
-            var templeID = temples[i] + "closureList";
-            console.log(templeID);
+            templeID = temples[i] + "closureList";
     
             var allData = jsonObj["temples"];
                 for (var z = 0; z < allData.length; z++){
-                    var number = allData[z].number;
+                   /* var number = allData[z].number;*/
                     var name = allData[z].name;
     
                         if (name === temples[i]){
                   
 
-                            for( var x = 0; x < allData[z].closures.length; x++){
+                            for( var x = 0; allData[z].closures.length; x++){
                                 var event = document.createElement("p");
                                 event.textContent = allData[z].closures[x]; 
                                 document.getElementById(templeID).appendChild(event);
